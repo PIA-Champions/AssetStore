7ad3859550a76f0a167d5e2c764d64559be831ef
lsantanar <contatolsr@hotmail.com>
2023-11-05 18:50:59 -0300

    lógica de exibição da search baseado no breakpoint

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -3,23 +3,41 @@ import styles from "./header.module.css";
 import HeaderLink from "../HeaderLink";
 import HeaderSearch from "../HeaderSearch";
 
-function Header() {
-    return (
-        <header className={styles.header}>
-            <Link to="./">
-                <h1>
-                    ASSET STORE
-                </h1>
-            </Link>
-            <nav>
-                <HeaderLink url="./">Home</HeaderLink>
-                <HeaderLink url="./about">About</HeaderLink>
-                <HeaderLink url="./assets">Assets</HeaderLink>
-                {/* <HeaderSearch /> */}
-            </nav>
-        </header>
+const dimension = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
-    )
+function Header() {
+    if (dimension > 1280) {
+        return (
+            <header className={styles.header}>
+                <Link to="./">
+                    <h1>
+                        ASSET STORE
+                    </h1>
+                </Link>
+                <nav>
+                    <HeaderLink url="./">Home</HeaderLink>
+                    <HeaderLink url="./about">About</HeaderLink>
+                    <HeaderLink url="./assets">Assets</HeaderLink>
+                    <HeaderSearch />
+                </nav>
+            </header>
+        )
+    } else {
+        return (
+            <header className={styles.header}>
+                <Link to="./">
+                    <h1>
+                        ASSET STORE
+                    </h1>
+                </Link>
+                <nav>
+                    <HeaderLink url="./">Home</HeaderLink>
+                    <HeaderLink url="./about">About</HeaderLink>
+                    <HeaderLink url="./assets">Assets</HeaderLink>
+                </nav>
+            </header>
+        )
+    }
 }
 
 export default Header;
--- frontend/src/components/HeaderSearch/index.js
+++ frontend/src/components/HeaderSearch/index.js
@@ -4,7 +4,7 @@ function HeaderSearch() {
     return (
         <form className='form-search'>
             <input type="text" placeholder="Search assets" />
-            <button type="submit">Search</button>
+            <button type="submit">Ok</button>
         </form>
     )
 }
