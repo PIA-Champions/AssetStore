38459199dfe226052de93d08a3d4cd09dabaf74b
Sobrinho <davidhiago@gmail.com>
2023-11-14 20:37:27 -0300

    Moving to chalicelib directory

---
+++ backend/api/chalicelib/definitions/database_defs.py
@@ -0,0 +1,4 @@
+#Define names and parameters for the database
+
+USER_TABLE_NAME = 'Teste_user'
+ASSET_PACK_TABLE_NAME = 'Teste_asset_pack' 
--- backend/api/definitions/database_defs.py
+++
@@ -1,4 +0,0 @@
-#Define names and parameters for the database
-
-USER_TABLE_NAME = 'user_table'
-ASSET_PACK_TABLE_NAME = 'asset_pack_table' 
