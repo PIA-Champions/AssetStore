23436491d5bccbd54f64e8b4eee2c2dd0b7ff582
lsantanar <contatolsr@hotmail.com>
2023-11-27 20:26:18 -0300

    Add Menu Dropdown

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -2,20 +2,18 @@ import { Link } from "react-router-dom";
 import styles from "./header.module.css";
 import HeaderLink from "../HeaderLink";
 import HeaderSearch from "../HeaderSearch";
+import MenuDropdown from "../MenuDropdown";
 
 function Header() {
     return (
         <header className={styles.header}>
             <Link to="./">
-                <h1>
-                    ASSET STORE
-                </h1>
+                <h1>ASSET STORE</h1>
             </Link>
             <nav>
                 <HeaderLink url="./">Home</HeaderLink>
                 <HeaderLink url="./assets">Assets</HeaderLink>
-                <HeaderLink url="./cadastro">Cadastro</HeaderLink>
-                <HeaderLink url="./login">Login</HeaderLink>
+                <MenuDropdown />
                 <HeaderSearch />
             </nav>
         </header>
---
+++ frontend/src/components/MenuDropdown/index.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import styles from "./MenuDropdown.module.css"
+
+function MenuDropdown() {
+    return (
+        <div className={styles.dropdown}>
+            <span className={styles.dropbtn} typeof="button" role="button">Área do usuário</span>
+            <div className={styles.dropdown_content}>
+                <Link to="./cadastro">Cadastro</Link>
+                <Link to="./login">Login</Link>
+            </div>
+        </div>
+    );
+}
+
+export default MenuDropdown;
