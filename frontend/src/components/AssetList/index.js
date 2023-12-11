93287120e19e911f7692def9cde35b86bd4794a6
graviolagames <graviolagames@gmail.com>
2023-12-11 14:56:04 -0300

    [WIP] Access list of assets from api

--- frontend/src/components/AssetList/index.js
+++ frontend/src/components/AssetList/index.js
@@ -1,22 +1,31 @@
-//Component for listing all assets
+import React, { useState, useEffect } from 'react';
 import { apiUrl } from '../../ApiConfig/apiConfig.js';
+//import { AssetDetails } from '../AssetDetails/index.js';
 
-// Method for fetching assets from backend
-export default function  AssetList(){
-    try {
-        fetch('${apiUrl}/assets',{method:'GET'})
-        .then (response => response.json())
-        .then (data => console.log(data));
+export default function AssetList() {
+  const [assets, setAssets] = useState([]);
 
-        return (
-          <div>
-            <h2>Lista de Assets</h2>
-            <ul>
-                
-            </ul>
-          </div>
-        );
-    } catch (error) {
-      console.error('Error fetching assets:', error);
-    }
-};
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${apiUrl}/assets`, { method: 'GET' });
+        const data = await response.json();
+        console.log(data);
+        setAssets(data);
+      } catch (error) {
+        console.error('Error fetching assets:', error);
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array ensures that useEffect runs only once after the initial render
+
+  return (
+    <div>
+      <h2>Lista de Assets</h2>
+      <ul>
+        " todo "
+      </ul>
+    </div>
+  );
+}
