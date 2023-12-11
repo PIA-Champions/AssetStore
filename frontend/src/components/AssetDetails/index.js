be75e81ae4f61b260e1da52c089b0b18b8365a6a
lsantanar <contatolsr@hotmail.com>
2023-12-10 23:23:55 -0300

    edição dos assets

--- frontend/src/components/AssetDetails/index.js
+++ frontend/src/components/AssetDetails/index.js
@@ -4,7 +4,7 @@ function AssetDetails() {
     return (
         <div className={styles.card_container}>
             <h1 id="title-assets">RPG Asset</h1>
-            <figure id="image-assets"/>
+            <figure className={styles.image_card}/>
             <h3 id="description-assets">Descrição do produto aqui! Descrição do produto aqui! Descrição do produto aqui! Descrição do produto aqui!</h3>
             <h2 id="price-assets">R$50,00</h2>
             <button className="btn_assets">Comprar</button>
