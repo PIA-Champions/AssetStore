e9903efac6945553b6de6cada0d8b5c14a986c9e
graviolagames <graviolagames@gmail.com>
2023-12-11 13:52:59 -0300

    [WIP] Access list of assets from api

--- frontend/src/components/AssetList/index.js
+++ frontend/src/components/AssetList/index.js
@@ -1,38 +1,22 @@
 //Component for listing all assets
-import React, { useState, useEffect } from 'react';
 import { apiUrl } from '../../ApiConfig/apiConfig.js';
 
-const AssetList = () => {
-  const [assets, setAssets] = useState([]);
-
-  useEffect(() => {
-    // Method for fetching assets from backend
-    const fetchAssets = async () => {
-      try {
+// Method for fetching assets from backend
+export default function  AssetList(){
+    try {
         fetch('${apiUrl}/assets',{method:'GET'})
         .then (response => response.json())
-        .then (data => setAssets(data));
-      } catch (error) {
-        console.error('Error fetching assets:', error);
-      }
-    };
+        .then (data => console.log(data));
 
-    // Call the method for fetching assets when the component is mounted
-    fetchAssets();
-  }, []); // The empty array assures the code will be called one time only
-
-  return (
-    <div>
-      <h2>Lista de Assets</h2>
-      <ul>
-        {assets.map((asset) => (
-          <li key={asset.id}>
-            <strong>{asset.title}</strong> - {asset.description}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+        return (
+          <div>
+            <h2>Lista de Assets</h2>
+            <ul>
+                
+            </ul>
+          </div>
+        );
+    } catch (error) {
+      console.error('Error fetching assets:', error);
+    }
 };
-
-export default AssetList;
