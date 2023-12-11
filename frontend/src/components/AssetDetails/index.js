5f1b22a3972996ec91a18e7cbdc9f1b9d1a276d8
lsantanar <contatolsr@hotmail.com>
2023-12-10 21:03:31 -0300

    assets home and details

--- frontend/src/components/AssetDetails/index.js
+++ frontend/src/components/AssetDetails/index.js
@@ -4,7 +4,7 @@ function AssetDetails() {
     return (
         <div className={styles.card_container}>
             <h1 id="title-assets">RPG Asset</h1>
-            <picture id="image-assets"/>
+            <figure id="image-assets"/>
             <h3 id="description-assets">Descrição do produto aqui! Descrição do produto aqui! Descrição do produto aqui! Descrição do produto aqui!</h3>
             <h2 id="price-assets">R$50,00</h2>
             <button className="btn_assets">Comprar</button>
