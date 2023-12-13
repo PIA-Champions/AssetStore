d3c61f5c8d91b895f5b3ad3b107de43fe0f52007
graviolagames <graviolagames@gmail.com>
2023-12-13 19:16:43 -0300

    Update api endpoint

--- frontend/src/ApiConfig/apiConfig.js
+++ frontend/src/ApiConfig/apiConfig.js
@@ -1,2 +1,2 @@
 // Exports endpoints
-export const apiUrl = 'https://p9p369t733.execute-api.us-east-1.amazonaws.com/api';
+export const apiUrl = 'https://y5foywt2q5.execute-api.us-east-1.amazonaws.com/api';
