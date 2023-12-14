f4792e9b39e6d52689577f23a6f767cab6e8d071
graviolagames <graviolagames@gmail.com>
2023-12-13 22:33:05 -0300

    Update api address after backend deploy

--- frontend/src/ApiConfig/apiConfig.js
+++ frontend/src/ApiConfig/apiConfig.js
@@ -1,2 +1,2 @@
 // Exports endpoints
-export const apiUrl = 'https://2h6ux7gsm8.execute-api.us-east-1.amazonaws.com/api';
+export const apiUrl = 'https://t5l3p1jir2.execute-api.us-east-1.amazonaws.com/api';
